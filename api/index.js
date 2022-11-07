ed6dfb0cc25b1848b3387c570d94ef00c8ef4189
Farid <fargmov@gmail.com>
2022-11-07 21:45:03 +0300

    api index js production code remove

--- api/index.js
+++ api/index.js
@@ -25,21 +25,6 @@ app.use('/api/products', productRoute)
 app.use('/api/orders', orderRoute)
 app.use('/api/checkouts', stripeRoute)
 
-
-// Serve frontend
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../','client', 'build', 'index.html'))
-    })
-} else {
-    app.get('/', (req, res) => {
-        res.send('Please set to production!')
-    })
-}
-
-
 app.listen(port, () => {
     console.log(`Starting server on port ${port}`)
 })
