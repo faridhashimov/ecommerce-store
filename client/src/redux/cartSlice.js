984fa2df3330b87718ebbbe750ed0786e7c3b4f5
Farid <fargmov@gmail.com>
2023-01-04 15:58:10 +0300

    refactor add product checking when add or inc qt

--- client/src/redux/cartSlice.js
+++ client/src/redux/cartSlice.js
@@ -6,33 +6,53 @@ export const cartSlice = createSlice({
         products: [],
     },
     reducers: {
-        addToCart: (state, action) => {
-            state.products.push(action.payload)
-            state.totalSum = action.payload.price * action.payload.quantity
+        addToCart: (state, { payload }) => {
+            if (
+                state.products.some(
+                    (product) =>
+                        product.id === payload.id &&
+                        product.productColor === payload.productColor &&
+                        product.productSize === payload.productSize
+                )
+            ) {
+                state.products.map((product) =>
+                    product.id === payload.id &&
+                    product.productColor === payload.productColor &&
+                    product.productSize === payload.productSize
+                        ? ((product.quantity += payload.quantity),
+                          (product.total = product.quantity * product.price))
+                        : product
+                )
+            } else {
+                state.products.push(payload)
+            }
         },
-        deleteFromCart: (state, action) => {
+        deleteFromCart: (
+            state,
+            { payload: { id, productColor, productSize } }
+        ) => {
             state.products = state.products.filter(
                 (product) =>
-                    product.id !== action.payload.id ||
-                    product.productColor !== action.payload.productColor ||
-                    product.productSize !== action.payload.productSize
+                    product.id !== id ||
+                    product.productColor !== productColor ||
+                    product.productSize !== productSize
             )
         },
-        increaseQt: (state, action) => {
+        increaseQt: (state, { payload: { id, productColor, productSize } }) => {
             state.products.forEach((product) =>
-                product.id === action.payload.id &&
-                product.productColor === action.payload.productColor &&
-                product.productSize === action.payload.productSize
+                product.id === id &&
+                product.productColor === productColor &&
+                product.productSize === productSize
                     ? ((product.quantity += 1),
                       (product.total = product.quantity * product.price))
                     : product
             )
         },
-        decreaseQt: (state, action) => {
+        decreaseQt: (state, { payload: { id, productColor, productSize } }) => {
             state.products.forEach((product) =>
-                product.id === action.payload.id &&
-                product.productColor === action.payload.productColor &&
-                product.productSize === action.payload.productSize
+                product.id === id &&
+                product.productColor === productColor &&
+                product.productSize === productSize
                     ? ((product.quantity -= 1),
                       (product.total = product.total - product.price))
                     : product
