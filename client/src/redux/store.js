4046874a45ed92c10bd724654f99c1fe24a980ff
Farid <fargmov@gmail.com>
2023-01-12 22:45:51 +0300

    feature remove redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,53 +1,20 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice'
 import wishlistReducer from './wishlistSlice'
 import cartReducer from './cartSlice'
 import resetReducer from './resetSlice'
 
-import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist'
-
-import storage from 'redux-persist/lib/storage'
-
-const persistConfig = {
-    key: 'root',
-    version: 1,
-    storage,
-}
-
-const rootReducer = combineReducers({
-    user: userReducer,
-    wishlist: wishlistReducer,
-    cart: cartReducer,
-    reset: resetReducer,
-})
-
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+import { ecommerceApi } from './ecommerceApi/ecommerceApi'
 
 export const store = configureStore({
-    reducer: persistedReducer,
+    reducer: {
+        user: userReducer,
+        wishlist: wishlistReducer,
+        cart: cartReducer,
+        reset: resetReducer,
+        [ecommerceApi.reducerPath]: ecommerceApi.reducer,
+    },
 
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [
-                    FLUSH,
-                    REHYDRATE,
-                    PAUSE,
-                    PERSIST,
-                    PURGE,
-                    REGISTER,
-                ],
-            },
-        }),
+        getDefaultMiddleware().concat(ecommerceApi.middleware),
 })
-
-export let persistor = persistStore(store)
