363aa5aa790e91f561e5dcd608eeb12bcb9fa722
Farid <fargmov@gmail.com>
2023-01-20 20:48:41 +0300

    reafactor change slice after adding rtk query

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -3,31 +3,17 @@ import { createSlice } from '@reduxjs/toolkit'
 export const userSlice = createSlice({
     name: 'user',
     initialState: {
-        pending: null,
-        user: null,
-        error: false,
+        user: JSON.parse(localStorage.getItem('userData')) || {},
     },
     reducers: {
-        loginStart: (state) => {
-            state.pending = true
-            state.error = false
-        },
-        loginSuccess: (state, action) => {
-            state.pending = false
+        userLogin: (state, action) => {
             state.user = action.payload
-            state.error = false
-        },
-        loginFailure: (state) => {
-            state.pending = false
-            state.error = true // state.error = action.payload
         },
         logOut: (state) => {
-            state.pending = false
             state.user = null
-            state.error = false
-        }
+        },
     },
 })
 
-export const { loginStart, loginSuccess, loginFailure, logOut } = userSlice.actions
+export const { userLogin, logOut } = userSlice.actions
 export default userSlice.reducer
