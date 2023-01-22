302ade95fff5d15f9f27e3e68af366942d7686a8
Farid <fargmov@gmail.com>
2023-01-22 17:29:54 +0300

    container width changes

--- client/src/components/ErrorMsg.jsx
+++ client/src/components/ErrorMsg.jsx
@@ -2,14 +2,15 @@ import styled from 'styled-components'
 
 const Container = styled.div`
     height: 100%;
-    width: 100%;
+    width: ${(props) => (props.width ? props.width : 100)}%;
     display: flex;
+    text-align: center;
     justify-content: center;
     align-items: center;
     color: red;
 `
-const ErrorMsg = ({ error }) => {
-    return <Container>Something went wrong: {error}...</Container>
+const ErrorMsg = ({ error, width }) => {
+    return <Container width={width}>Something went wrong: {error}...</Container>
 }
 
 export default ErrorMsg
