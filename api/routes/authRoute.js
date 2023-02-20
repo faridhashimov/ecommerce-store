2e0804026537c1bdaa466f9ce6db22349b20ff88
Farid <fargmov@gmail.com>
2023-02-20 22:26:23 +0300

    add new routes

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -2,9 +2,13 @@ const router = require('express').Router()
 const {
     userRegister,
     userLogin,
+    refresh,
+    logOut,
 } = require('../controllers/authController')
 
 router.post('/register', userRegister)
 router.post('/login', userLogin)
+router.get('/refresh', refresh)
+router.post('/logOut', logOut)
 
 module.exports = router
